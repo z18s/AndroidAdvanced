77b9de5a61cdc861a659049baf02e343d5f8ada3
Konstantin <57114608+z18s@users.noreply.github.com>
2020-08-07 16:30:25 +0300

    WeatherRequest init

--- app/src/main/java/com/example/weatherapp/model/weather/WeatherRequest.java
+++ app/src/main/java/com/example/weatherapp/model/weather/WeatherRequest.java
@@ -1,17 +1,159 @@
 package com.example.weatherapp.model.weather;
 
-import java.util.Random;
+import com.example.weatherapp.model.data.Clouds;
+import com.example.weatherapp.model.data.Coord;
+import com.example.weatherapp.model.data.Main;
+import com.example.weatherapp.model.data.Sys;
+import com.example.weatherapp.model.data.Weather;
+import com.example.weatherapp.model.data.Wind;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
-// CLASS FOR TEST
+import java.util.List;
+
 public class WeatherRequest {
-    private WeatherValues weatherValues;
 
-    public WeatherRequest() {
-        int temp = new Random().nextInt(30);
-        weatherValues = new WeatherValues(temp);
+    @SerializedName("coord")
+    @Expose
+    private Coord coord;
+    @SerializedName("weather")
+    @Expose
+    private List<Weather> weather = null;
+    @SerializedName("base")
+    @Expose
+    private String base;
+    @SerializedName("main")
+    @Expose
+    private Main main;
+    @SerializedName("visibility")
+    @Expose
+    private Integer visibility;
+    @SerializedName("wind")
+    @Expose
+    private Wind wind;
+    @SerializedName("clouds")
+    @Expose
+    private Clouds clouds;
+    @SerializedName("dt")
+    @Expose
+    private Integer dt;
+    @SerializedName("sys")
+    @Expose
+    private Sys sys;
+    @SerializedName("timezone")
+    @Expose
+    private Integer timezone;
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("cod")
+    @Expose
+    private Integer cod;
+
+    public Coord getCoord() {
+        return coord;
     }
 
-    public WeatherValues getWeatherValues() {
-        return weatherValues;
+    public void setCoord(Coord coord) {
+        this.coord = coord;
+    }
+
+    public List<Weather> getWeather() {
+        return weather;
+    }
+
+    public void setWeather(List<Weather> weather) {
+        this.weather = weather;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    public Integer getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(Integer visibility) {
+        this.visibility = visibility;
+    }
+
+    public Wind getWind() {
+        return wind;
+    }
+
+    public void setWind(Wind wind) {
+        this.wind = wind;
+    }
+
+    public Clouds getClouds() {
+        return clouds;
+    }
+
+    public void setClouds(Clouds clouds) {
+        this.clouds = clouds;
+    }
+
+    public Integer getDt() {
+        return dt;
+    }
+
+    public void setDt(Integer dt) {
+        this.dt = dt;
+    }
+
+    public Sys getSys() {
+        return sys;
+    }
+
+    public void setSys(Sys sys) {
+        this.sys = sys;
+    }
+
+    public Integer getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(Integer timezone) {
+        this.timezone = timezone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCod() {
+        return cod;
+    }
+
+    public void setCod(Integer cod) {
+        this.cod = cod;
     }
 }
